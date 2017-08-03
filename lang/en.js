1d72d5d1db27b3d983e18a1a37ffa2f4954227ad
Martin Amm <ma@adenin.com>
2017-08-02 20:32:48 -0400

    started localization

---
+++ lang/en.js
@@ -0,0 +1,9 @@
+
+! function() {
+  Polymer.atI18nLocales.add('at-carbon-card-title', 'en', {
+    'Unpin from workplace': 'Unpin from workplace',
+    'Pin to workplace': 'Pin to workplace'
+  });
+}();
+
+
