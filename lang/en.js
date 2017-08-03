6e5baf38cc17c10a86c731e3f0d052fe7dd9f6b0
Martin Amm <ma@adenin.com>
2017-08-03 18:35:15 -0400

    change menu entry

--- lang/en.js
+++ lang/en.js
@@ -2,7 +2,7 @@
 ! function() {
   Polymer.atI18nLocales.add('at-carbon-card-title', 'en', {
     'Unpin from workplace': 'Unpin from workplace',
-    'Pin to workplace': 'Pin to workplace'
+    'Pin {0} to workplace': 'Pin {0} to workplace'
   });
 }();
 
