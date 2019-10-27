d0dbf1dc939514a92951a1e7226298b28bb24e7a
Martin Amm <martin.amm@adenin.com>
2019-10-27 03:49:39 -0400

    changed icons, workplace -> My Board

--- lang/en.js
+++ lang/en.js
@@ -1,8 +1,8 @@
 
 ! function() {
   Polymer.atI18nLocales.add('at-carbon-card-title', 'en', {
-    'Unpin from workplace': 'Unpin from workplace',
-    'Pin {0} to workplace': 'Pin {0} to workplace'
+    'Unpin from My Board': 'Unpin from My Board',
+    'Pin {0} to My Board': 'Pin {0} to My Board'
   });
 }();
 
