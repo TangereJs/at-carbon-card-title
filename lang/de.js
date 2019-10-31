a4d6dac482adfe9d0b1c492ea5ee6b73289823cd
Martin Amm <martin.amm@adenin.com>
2019-10-31 16:09:19 -0400

    added de

---
+++ lang/de.js
@@ -0,0 +1,13 @@
+
+! function () {
+  Polymer.atI18nLocales.add('at-carbon-card-title', 'de', {
+    'Unpin from My Board': 'Von My Board entfernen',
+    'Pin {0} to My Board': 'An My Board heften',
+    'Configure Card': 'Karte konfigurieren',
+    'Dismiss': 'Gelesen',
+    'Unsubscribe': 'Keine weiteren Benachrichtigungen',
+    'Subscribe': 'Benachrichtigung aktivieren'
+  });
+}();
+
+
